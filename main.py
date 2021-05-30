0bccec0588bb432c6d641d22bd1cb3c82f968830
ariel415el <ariel415el@gmail.com>
2021-05-30 18:29:37 +0300

    Resizeable window

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ from consts import AIRLINES, AIRPORTS, LANGAUGES
 
 from utils import create_price_proposal, send_whatsapp, parse_amadeus_code
 
-W1 = 21
-W2 = 14
-W3 = 7
-
 TIMES = [f"{str(h).zfill(2)}:{str(m).zfill(2)}" for h in range(24) for m in range(0, 60,10)]
 TRAVELERS = ['Child', 'Adult']
 COMPARTEMENTS = ['1st Class', 'Business', 'Economy']
@@ -22,13 +18,13 @@ def get_travelers_rows(n, default_dict):
     for i in range(n):
         adult_default = not default_dict[f"child{i}"]
         row += [sg.Frame(f"Traveler {i + 1}", [
-                                           [sg.InputText(key=f"traveler_name{i}", size=(W2, 1), default_text=default_dict[f"traveler_name{i}"])],
+                                           [sg.InputText(key=f"traveler_name{i}", size=(W2, 1), default_text=default_dict[f"traveler_name{i}"],)],
                                            [sg.Radio(f"Adult", f"traveler_age{i}", key=f"adult{i}", default=adult_default)],
                                            [sg.Radio(f"Child", f"traveler_age{i}", key=f"child{i}", default=default_dict[f"child{i}"])]
                                           ])
                 ]
     row += [sg.Button('+', key="+traveler", size=(2, 1))]
-    phone_row = [sg.Text('Contact WhatsApp num', size=(W2, 1)),
+    phone_row = [sg.Text('Contact WhatsApp', size=(W1, 1)),
                  sg.Input(key="phone_number", size=(W1 - 1, 1), default_text=default_dict["phone_number"])]
     return [phone_row, row]
 
@@ -66,14 +62,14 @@ def get_flight_input_frame(i, default_dict):
             [sg.Text("Airline", size=(W2,1)), sg.Drop(AIRLINES, size=(W1, 1), key=f"airline{i}", default_value=default_dict[f"airline{i}"])],
             [sg.Text("Code", size=(W2,1)), sg.Input(size=(W1, 1), key=f"code{i}", default_text=default_dict[f"code{i}"])],
             [sg.Text("Seat", size=(W2,1)), sg.Input(key=f"seat{i}", size=(W1, 1), default_text=default_dict[f"seat{i}"])],
-            [sg.Text("Compartment", size=(W2,1)), sg.Drop(COMPARTEMENTS, key=f"compartment{i}", size=(W2 - 1, 1), default_value=compartement_default)]
+            [sg.Text("Compartment", size=(W2,1)), sg.Drop(COMPARTEMENTS, key=f"compartment{i}", size=(W1, 1), default_value=compartement_default)]
         ])
     ]]
     columns = [sg.Column(depart_col), sg.Column(dest_col), sg.Column(details_col)]
 
     # add an amadeus parser above the columns
-    amadeus_row = [sg.Text("Amadeus code:", size=(W2,1)),
-                   sg.Input(key=f"amadeus_code{i}", size=(W2*6,1), default_text=default_dict[f"amadeus_code{i}"]),
+    amadeus_row = [sg.Text("Amadeus code:", size=(W1,1)),
+                   sg.Input(key=f"amadeus_code{i}", size=(W2*8,1), default_text=default_dict[f"amadeus_code{i}"]),
                    sg.Button('Insert', key=f"insert_{i}")]
     rows = [amadeus_row, columns]
     return rows
@@ -93,19 +89,19 @@ def get_flight_rows_3(n, default_dict):
 def get_price_and_restriction_rows(default_dict):
     rows = [[
         sg.Frame('Price', [
-            [sg.Text("Price (adult)", size=(W2, 1)), sg.Input(size=(W3, 1), key="price_adult", default_text=default_dict["price_adult"])],
-            [sg.Text("Price (child)", size=(W2, 1)), sg.Input(size=(W3, 1), key="price_child", default_text=default_dict["price_child"])],
+            [sg.Text("Price (adult)", size=(W2, 1)), sg.Input(size=(PRICE_BOX_SIZE, 1), key="price_adult", default_text=default_dict["price_adult"])],
+            [sg.Text("Price (child)", size=(W2, 1)), sg.Input(size=(PRICE_BOX_SIZE, 1), key="price_child", default_text=default_dict["price_child"])],
             [sg.Text("fee percentage", size=(W2, 1)),
-             sg.Input(size=(W3, 1), key="price_fee", default_text=default_dict["price_fee"])]
+             sg.Input(size=(PRICE_BOX_SIZE, 1), key="price_fee", default_text=default_dict["price_fee"])]
         ]),
 
         sg.Frame('Restrictions', [
             [sg.Text("Change fee", size=(W2, 1)),
-             sg.Input(size=(W3, 1), key="change_fee", default_text=default_dict["change_fee"])],
+             sg.Input(size=(PRICE_BOX_SIZE, 1), key="change_fee", default_text=default_dict["change_fee"])],
             [sg.Text("Cancel fee", size=(W2, 1)),
-             sg.Input(size=(W3, 1), key="cancel_fee", default_text=default_dict["cancel_fee"])],
+             sg.Input(size=(PRICE_BOX_SIZE, 1), key="cancel_fee", default_text=default_dict["cancel_fee"])],
             [sg.Text("No show fee", size=(W2, 1)),
-             sg.Input(size=(W3, 1), key="no_show_fee", default_text=default_dict["no_show_fee"])],
+             sg.Input(size=(PRICE_BOX_SIZE, 1), key="no_show_fee", default_text=default_dict["no_show_fee"])],
         ]
                  )
     ]]
@@ -140,20 +136,23 @@ def get_total_layout(n_travelers, n_flights, default_values, preview_msg):
     layout.append([sg.Frame("Prices", get_price_and_restriction_rows(default_values)),
                    sg.Frame("Details", get_price_detail_rows(default_values))])
 
-    layout.append([sg.Button("Preview", key='preview'), sg.Button('WhatsApp', key='WhatsApp'), sg.Exit(),
-                   # sg.Button('en', key='en', size=(5,1)),
-                   # sg.Button('fr', key='fr', size=(5,1)),
-                   sg.ReadFormButton('', key='en', border_width=0, image_subsample=20,button_color=sg.TRANSPARENT_BUTTON, image_filename=os.path.join('images', 'united-states.png')),
-                   sg.ReadFormButton('', key='fr', border_width=0, image_subsample=20,button_color=sg.TRANSPARENT_BUTTON, image_filename=os.path.join('images', 'france.png')),
-                   # sg.ReadFormButton('', key='he', border_width=0, image_subsample=20,button_color=sg.TRANSPARENT_BUTTON, image_filename=os.path.join('images', 'israel.png')),
-                   sg.Text(LANGAUGES[default_values['lang']])])
+    layout.append([
+                   sg.Button('Preview', key='Preview'),
+                   sg.Frame('Terminals', [[sg.Button('WhatsApp', key='WhatsApp'), sg.Exit()]]),
+                   sg.Frame('Resolution', [[sg.Button('+', key='res+'), sg.Button('-', key='res-')]]),
+                   sg.Frame('Language', [[
+                       sg.ReadFormButton('', key='en', border_width=0, image_subsample=20,button_color=sg.TRANSPARENT_BUTTON, image_filename=os.path.join('images', 'united-states.png')),
+                       sg.ReadFormButton('', key='fr', border_width=0, image_subsample=20,button_color=sg.TRANSPARENT_BUTTON, image_filename=os.path.join('images', 'france.png')),
+                       # sg.ReadFormButton('', key='he', border_width=0, image_subsample=20,button_color=sg.TRANSPARENT_BUTTON, image_filename=os.path.join('images', 'israel.png')),
+                    sg.Text(LANGAUGES[default_values['lang']])]]),
+                   ])
 
     # Lay the input side by side to an output window
-    layout = [[sg.Column(layout), sg.Column([[sg.Multiline(size=(50, 35 + (n_flights-1)*12), default_text=preview_msg, key='output_window')]])]]
+    layout = [[sg.Column(layout), sg.Column([[sg.Multiline(size=(2*W1, 5*8 + (n_flights-1)*3*W2), default_text=preview_msg, key='output_window')]])]]
 
     return layout
 
-def main():
+def main(fs):
     """
     Run the main gui application that constantly seeks for user input and shows a preview on a side window.
     By pressing "Whatsapp" a link to whatsapp message is opened
@@ -170,7 +169,7 @@ def main():
 
         layout = get_total_layout(n_travelers, n_flights, default_values, preview_msg)
 
-        window = sg.Window('Simple data entry window', layout=layout)
+        window = sg.Window('Simple data entry window', layout=layout, font=(f"Arial {fs}"))
         event, values = window.read()
         default_values.update(values)
 
@@ -181,6 +180,10 @@ def main():
             n_flights += 1
         if event == 'WhatsApp':
             send_whatsapp(default_values['phone_number'], default_values['output_window'])
+        if event == 'res+':
+            fs = fs * 2
+        if event == 'res-':
+            fs = fs // 2
         if event.startswith('insert'):
             index = event.split("_")[-1]
             default_values.update(parse_amadeus_code(values[f"amadeus_code{index}"], index))
@@ -188,10 +191,18 @@ def main():
             default_values['lang'] = event
         if event in ['Exit', sg.WIN_CLOSED]:
             break
+        if event == 'Preview':
+            default_values['output_window'] = create_price_proposal(default_values)
 
-        default_values['output_window'] = create_price_proposal(default_values)
         window.close()
 
 
 if __name__ == '__main__':
-    main()
+    W1 = 20
+    W2 = 12
+    PRICE_BOX_SIZE = 7
+    fs = 10
+    w, h = sg.Window.get_screen_size()
+    w = int(w * 0.8)
+    h = int(h * 0.8)
+    main(fs)
--- test.py
+++ test.py
@@ -1,29 +1,17 @@
 import PySimpleGUI as sg
 
-sg.ChangeLookAndFeel('Dark')
-sg.SetOptions(element_padding=(0,0))
-
 layout = [
-          [sg.ReadFormButton('Numbers', button_color=('white', 'black'), key='start'),
-           sg.ReadFormButton('Letters', button_color=('white', 'firebrick4'), key='stop')],
-          [sg.Combo(('Numbers', 'Letters'), key='combo1', change_submits=True), sg.Combo(('a', 'b', 'c', 'd'), key='combo2')]
-          ]
+    [sg.Frame("I'm a frame",[[sg.Button(button_text='Change size')]],key='body')]
+]
 
-form = sg.FlexForm("Change Combo Values", default_element_size=(12,1), text_justification='r', auto_size_text=False, auto_size_buttons=False,
-                   default_button_element_size=(12,1))
-form.Layout(layout)
+window = sg.Window('Window',layout)
+window.finalize()
+
+
 while True:
-    button, values = form.Read()
-    if button is None:
-        exit(69)
-    if button is 'Numbers':
-        form.FindElement('combo1').Update(values=(7,8,9,0))
-        form.FindElement('combo2').Update(values=(1,2,3,4))
-    elif button is 'Letters':
-        form.FindElement('combo1').Update(values=('a','b','c','d'))
-        form.FindElement('combo2').Update(values=('e','f','g','h'))
-
-    if values['combo1'] == 'Numbers':
-        form.FindElement('combo2').Update(values=(1,2,3,4))
-    elif values['combo1'] == 'Letters':
-        form.FindElement('combo2').Update(values=('e','f','g','h'))
+    event, values = window.read()
+    if event in (None, 'Cancel'):
+        break
+    if event == 'Change size':
+        window['Change size'].set_size(size=(200, 200))
+        window.refresh()
